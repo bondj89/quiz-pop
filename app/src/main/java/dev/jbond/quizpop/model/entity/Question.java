417117317cdab36eb3fb85df15eef497bd6b07c4
Bond <jobocuban@gmail.com>
2019-10-18 10:51:43 -0600

    Updated ERDs

--- app/src/main/java/dev/jbond/quizpop/model/entity/Question.java
+++ app/src/main/java/dev/jbond/quizpop/model/entity/Question.java
@@ -8,17 +8,6 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(
-    foreignKeys = {
-        @ForeignKey(
-            entity = Category.class,
-            parentColumns = {"category_id"},
-            childColumns = {"category_id"},
-            onDelete = ForeignKey.CASCADE
-        )
-    }
-)
-
 public class Question {
 
   @PrimaryKey(autoGenerate = true)

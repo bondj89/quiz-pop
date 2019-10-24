06e6ccc35a9775cb59763b9a39eb3aae0638a60e
Bond <jobocuban@gmail.com>
2019-10-23 22:30:22 -0600

    Partially implemented Google Sign in.

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 ## Introduction 
-For my personal android project, I chose to create an app called QuizPop. 
+For this android project, I chose to create an app called QuizPop. 
 The app is simple and will consist of multiple choice, true or false type 
 questions of general trivia knowledge. 
 
 
 ## Description 
-Developed in Android, QuizPop is an all you can play trivia game that 
-allows players to answer questions from their favorite topics. 
-Players can choose questions in the format of multiple choice and true or false. 
-Players can choose the difficulty level and compete against their 
-friends by signing in. 
+Developed in Android, QuizPop is an all you can play trivia game 
+that allows players to answer questions from their favorite topics. 
+Questions are in the format of multiple choice and true or false. 
+Players can choose the difficulty level and compete against friends 
+by signing in through their Google account. 
 
 ## Intended Users 
 Anyone that loves to play trivia from the palm of their hands. 
-This app should appeal to people of all ages who enjoy trivia related activities, 
-flexing their brain and competing with friends. 
+This app should appeal to people of all ages who enjoy trivia 
+related activities, flexing their brain and competing with friends. 
 
 ### [User Stories](docs/user-stories.md)
 
--- app/src/main/java/dev/jbond/quizpop/controller/MainActivity.java
+++ app/src/main/java/dev/jbond/quizpop/controller/MainActivity.java
@@ -1,12 +1,15 @@
 package dev.jbond.quizpop.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 import dev.jbond.quizpop.R;
+import dev.jbond.quizpop.service.GoogleSignInService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +43,34 @@ public class MainActivity extends AppCompatActivity {
     mTextMessage = (TextView) findViewById(R.id.message);
     BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
     navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+  }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.sign_out:
+        signOut();
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+    }
+    return handled;
+  }
+
+  private void signOut() {
+    GoogleSignInService.getInstance().signOut()
+        .addOnCompleteListener((task) -> {
+          Intent intent = new Intent(this, LoginActivity.class);
+          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+        });
   }
 
 }

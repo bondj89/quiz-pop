ab9d7c42e7d658f3331fa2e33ac5126729e81b7d
Bond <jobocuban@gmail.com>
2019-11-14 14:38:14 -0700

    edited questionfragment xml and updated mainViewModel

--- app/src/main/java/dev/jbond/quizpop/MainViewModel.java
+++ app/src/main/java/dev/jbond/quizpop/MainViewModel.java
@@ -48,36 +48,6 @@ public class MainViewModel extends AndroidViewModel {
   }
 
 
-//  public void createRandomQuestion() {
-//    QuizPopService.getInstance().randomQuestion(DEFAULT_NUMBER_OF_QUESTIONS)
-//        .subscribeOn(Schedulers.from(executor))
-//        .subscribe((response -> {
-//          for (Question question : response.getQuestions()) {
-//            if (question.getType() == Type.BOOLEAN) {
-//              question.setCorrect(Boolean.valueOf(question.getTempCorrectAnswer()));
-//              database.getQuestionDao().insert(question);
-//            } else {
-//              List<Answer> answers = new LinkedList<>();
-//              Answer answer = new Answer();
-//              answer.setText(question.getTempCorrectAnswer());
-//              answer.setCorrect(true);
-//              answers.add(answer);
-//              for (String s : question.getTempIncorrectAnswers()) {
-//                answer = new Answer();
-//                answer.setText(s);
-//                answer.setCorrect(false);
-//                answers.add(answer);
-//              }
-//              long id = database.getQuestionDao().insert(question);
-//              for (Answer a : answers) {
-//                a.setQuestionId(id);
-//              }
-//              database.getAnswerDao().insert(answers);
-//            }
-//          }
-//        }));
-//  }
-
   public LiveData<List<Question>> getRandQuestionList() {
 
     return randQuestionList;
